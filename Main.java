704e9906c5b993a696782558eada927c6da46d55
Moe1711 <abdomoha590@gotvdsb.ca>
2020-10-05 14:23:28 +0000

    questions done done

--- Main.java
+++ Main.java
@@ -12,10 +12,12 @@ class Main {
   System.out.println("A keyboard is an exapmle of which of these. (Keep in mind it's case sensitive for all questions");
   System.out.println(" A. Softwear\n B. Hard drive \n C. Acessory \n D. Hardwear");
   char  choice1  = input.next().charAt(0);
+  int correct = 0;
 
   if (choice1=='D') { 
       System.out.println("Correct");
       ansOne = "Correct";
+      correct = correct+1;
     }
 
   else{
@@ -31,10 +33,12 @@ class Main {
     System.out.println("What does SSD stand for.");
     System.out.println(" A. Solid State Drive \n B. Solid State Device \n C. Smart Soft Drive \n D. Sovit Superior Device");
     char  choice2  = input.next().charAt(0);
+    int correct = 0;
 
     if  (choice2=='A') { 
       System.out.println("Correct");
       ansTwo = "Correct";
+      correct = correct+1;
     }
 
     else{
@@ -48,15 +52,18 @@ class Main {
     System.out.println("Why is an SSD superiour than aHard Drive.");
     System.out.println(" A. It's Stronger \n B. It's Faster \n C. It's Smaller \n D. It runs colder");
     char  choice3  = input.next().charAt(0);
+    int correct = 0;
 
     if (choice3=='B') { 
       System.out.println("Correct");
       ansThree = "Correct";
+      correct = correct+1;
     }
 
     else if (choice3=='C') { 
       System.out.println("Correct");
       ansThree = "Correct";
+      correct = correct+1;
     }
 
     else{
@@ -70,10 +77,12 @@ class Main {
     System.out.println("How do you input a decimal into your code.");
     System.out.println(" A. char \n B. int \n C. float \n D. String");
     char  choice4  = input.next().charAt(0);
+    int correct = 0;
 
     if (choice4=='C') { 
       System.out.println("Correct");
       ansFour = "Correct";
+      correct = correct+1;
     }
 
     else{
@@ -87,10 +96,12 @@ class Main {
     System.out.println("_____ reduces your carbon footprint.");
     System.out.println(" A. Recycling \n B. Driving \n C. Throwing out \n D. Hoarding");
     char  choice5  = input.next().charAt(0);
+    int correct = 0;
 
     if (choice5=='A') { 
       System.out.println("Correct");
       ansFive = "Correct";
+      correct = correct+1;
     }
 
     else{
@@ -104,10 +115,12 @@ class Main {
     System.out.println("_____ is gloabal warming.");
     System.out.println(" A. A rise in the planets average tempature \n B. Bad weather \n C. Snow \n D. Heatwaves");
     char  choice6  = input.next().charAt(0);
-
+    int correct = 0;
+    
     if (choice6=='A') { 
       System.out.println("Correct");
       ansSix = "Correct";
+      correct =correct+1;
     }
 
     else{
@@ -121,10 +134,12 @@ class Main {
     System.out.println("What will this line of code output: System.out.print(\"Hi\");");
     System.out.println("A. Hello \n B. Bye \n C. Hi \n D. Bonjour");
     char  choice7  = input.next().charAt(0);
+    int correct = 0;
 
     if (choice7=='C') { 
       System.out.println("Correct");
       ansSeven = "Correct";
+      correct = correct+1;
     }
 
     else{
@@ -138,10 +153,12 @@ class Main {
     System.out.println("What does this operator mean: \"==\".");
     System.out.println("A. Equal to \n B. More than \n C. Less than \n D. Divided by");
     char  choice8  = input.next().charAt(0);
+    int correct = 0;
 
     if (choice8=='A') { 
       System.out.println("Correct");
       ansEight = "Correct";
+      correct = correct+1;
     }
 
     else{
@@ -155,10 +172,12 @@ class Main {
     System.out.println("What is the operator : \"char\" used for");
     System.out.println("A. A number \n B. A decimal \n C. A Character \n D. A Sentance");
     char  choice9  = input.next().charAt(0);
+    int correct = 0;
 
     if (choice9=='C') { 
       System.out.println("Correct");
       ansNine = "Correct";
+      correct = correct+1;
     }
 
     else{
@@ -172,21 +191,26 @@ class Main {
     System.out.println("How many gigabytes are in a terabyte");
     System.out.println("A. 1000 \n B. 1500 \n C. 850 \n D. 1024");
     char  choice10  = input.next().charAt(0);
+     int correct = 0;
 
     if (choice10=='A') { 
       System.out.println("Correct");
       ansTen = "Correct";
+      correct = correct+1;
     }
 
     else if (choice10=='D') { 
       System.out.println("Correct");
       ansTen = "Correct";
+      correct = correct+1;
     }
 
     else{
       System.out.println("Incorrect");
       ansTen="invalid selection";
     }
+
+
 
     
 
